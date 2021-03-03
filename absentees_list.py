3e6031d5927709e2b30981cab681f3c3e9fe808c
aswarth123 <c.v.aswarth@gmail.com>
2021-03-03 10:28:44 +0530

    Completed

--- absentees_list.py
+++ absentees_list.py
@@ -9,34 +9,59 @@ def name_conversion(f):
         csv_file = csv.DictReader(file)
         i=1
         for row in csv_file:
+            temp = []
             if(len(row['Roll no.'])==0):
                 continue
-            elif(i<10):
-                names.append(row['Full Name']+'-'+'AM.EN.U4CSE1900'+row['Roll no.'])
+            elif(i<9):
+                temp.append(row['Full Name']+'-'+'AM.EN.U4CSE1900'+row['Roll no.'])
             else:
-                names.append(row['Full Name']+'-'+'AM.EN.U4CSE190'+row['Roll no.'])
+                temp.append(row['Full Name']+'-'+'AM.EN.U4CSE190'+row['Roll no.'])
+            temp.append(0)
+            temp.append(0)
+            names.append(temp)
+            del temp
+            i+=1
     return(names)
 
 def absentees_list_generator(names,attcsv,classDur,percent_att):
     with open("attendenceLists/"+ attcsv, 'r',encoding='utf16', errors='ignore') as file:
         csv_file = csv.DictReader(file)
         i=1
-        final_list = []
+        att_list = []
         for row in csv_file:
             temp = (row['Full Name\tUser Action\tTimestamp']+row[None][0]).split('\t')
             date = temp.pop()
             format = '%m/%d/%Y %H:%M:%S %p'
             obj_date = datetime.datetime.strptime(date, format)
             temp.append(obj_date)
-            final_list.append(temp)
-    '''df = pd.read_csv("attendenceLists/"+ attcsv)
-    print(df.head())'''
-    print(final_list)
-    
+            att_list.append(temp)
+
+    for row in att_list:
+        if('-' not in row[0]):
+            teacher_tstamp = row[2]
+            print(teacher_tstamp)
+        else:
+            f=0
+            for i in range(0,len(names)):
+                if(names[i][0]==row[0]):
+                    f=1
+                    break
+            if(f==1 and (names[i][1]==0 or names[i][1]==-1) and row[1]=="Joined"):
+                names[i][1] = row[2]
+            elif(f==1 and row[1]=="Left"):
+                names[i][2] += (((row[2]-names[i][1]).seconds)//60)
+                names[i][1] = -1
+    class_end = teacher_tstamp+datetime.timedelta(hours = classDur)
+    for name in names:
+        if(name[1]!=-1 and name[1]!=0):
+            name[2] += (((class_end - name[1]).seconds)//60)
+
+    for name in names:
+        if(name[2]<((percent_att*classDur*60)//100)):
+            print(name[0],name[2])
+
 
 if __name__ == "__main__":
     names = name_conversion("classList/names.csv")
-    print(names[5])
-    print(names[15])
     attcsv = input()
-    absentees_list_generator([],attcsv,1,70)
+    absentees_list_generator(names,attcsv,2,70)
